1db7fbb80c7965324d3156022d1c98a9b3a3ecff
Mykhailo777 <mishahil2000@gmail.com>
2022-06-06 23:24:13 +0300

    completed

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,44 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  textarea: document.querySelector(' textarea'),
+  input: document.querySelector(' input'),
+};
+
+
+refs.form.addEventListener('input', throttle(onInputValue, 500));
+refs.form.addEventListener('submit', onFormSubmit);
+
+function onInputValue(e) {
+  const formsData = {
+    email: refs.input.value,
+    message: refs.textarea.value,
+  };
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formsData));
+}
+
+function dataForms() {
+    const stringifiedData = localStorage.getItem(STORAGE_KEY);
+    
+    
+  try {
+    const formsKey = JSON.parse(stringifiedData) || '';
+    if (formsKey) {
+      refs.input.value = formsKey.email;
+      refs.textarea.value = formsKey.message;
+    }
+  } catch (error) {
+    console.log('error');
+  }
+}
+formsDataCompleted();
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  console.log(localStorage.getItem(STORAGE_KEY));
+  localStorage.removeItem(STORAGE_KEY);
+  event.currentTarget.reset();
+}
