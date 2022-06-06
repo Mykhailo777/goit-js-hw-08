999365e8b7cdbb02655a1f00efe0da85d7397e6c
Mykhailo777 <mishahil2000@gmail.com>
2022-06-06 23:02:58 +0300

    fixed video play

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,2 +1,31 @@
 import Player from '@vimeo/player';
-import trottle from 'lodash.trottle';
+import throttle from 'lodash.throttle';
+ 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+player.on("timeupdate", throttle(onPlay, 1000));
+
+
+function onPlay(data) {
+    data = {
+         duration: 61.857,
+         percent: 0.049,
+         seconds: 3.034,
+    }
+    getCurrentTime();
+}
+
+function getCurrentTime() {
+  player.getCurrentTime().then(played => {
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(played));
+  });
+}
+const playerTime = localStorage.getItem('videoplayer-current-time');
+
+try {
+  let timesPlay = Number(JSON.parse(playerTime));
+  player.setCurrentTime(timesPlay);
+} catch (error) {
+  console.log('error');
+}
